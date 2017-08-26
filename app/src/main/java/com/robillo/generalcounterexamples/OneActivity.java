3631f64a779d466c76c99b81865f23ca096af1de
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-08-26 09:45:45 +0530

    One Activity Simple Layout

--- app/src/main/java/com/robillo/generalcounterexamples/OneActivity.java
+++ app/src/main/java/com/robillo/generalcounterexamples/OneActivity.java
@@ -2,6 +2,7 @@ package com.robillo.generalcounterexamples;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class OneActivity extends FragmentActivity {
 
@@ -9,5 +10,19 @@ public class OneActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one);
+
+        (findViewById(R.id.increment)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        (findViewById(R.id.decrement)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
