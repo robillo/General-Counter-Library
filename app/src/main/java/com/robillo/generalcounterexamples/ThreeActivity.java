85b3f0ed3e787afb91706973b7fa08ec7b216e01
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-08-29 14:07:58 +0530

    working on countdown activity

--- app/src/main/java/com/robillo/generalcounterexamples/ThreeActivity.java
+++ app/src/main/java/com/robillo/generalcounterexamples/ThreeActivity.java
@@ -1,14 +1,55 @@
 package com.robillo.generalcounterexamples;
 
+import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.robillo.generalcounterlibrary.GeneralCounter;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class ThreeActivity extends FragmentActivity {
+
+    int max;
+    boolean isZeroPlus = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_three);
+
+
+        (findViewById(R.id.countdown)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                max = Integer.valueOf(((EditText) findViewById(R.id.edit_text)).getText().toString());
+                if(max<=0){
+                    Toast.makeText(ThreeActivity.this, "NOT VALID VALUE; ENTER POSITIVE VALUES ONLY", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    while (max>0){
+                        Timer timer = new Timer();
+                        timer.scheduleAtFixedRate(new TimerTask() {
+                            @Override
+                            public void run() {
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        ((GeneralCounter) findViewById(R.id.general_counter)).decrement();
+                                        max--;
+                                    }
+                                });
+                            }
+                        }, 1000, 1000);
+                    }
+                }
+            }
+        });
     }
 }
