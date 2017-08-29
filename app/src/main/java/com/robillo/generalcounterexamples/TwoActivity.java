8ec24dd4ef70c7d2063c357bbb268bf38533ace4
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-08-29 13:57:39 +0530

    regular time counter implemented

--- app/src/main/java/com/robillo/generalcounterexamples/TwoActivity.java
+++ app/src/main/java/com/robillo/generalcounterexamples/TwoActivity.java
@@ -4,6 +4,11 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.robillo.generalcounterlibrary.GeneralCounter;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class TwoActivity extends FragmentActivity {
 
     @Override
@@ -14,14 +19,26 @@ public class TwoActivity extends FragmentActivity {
         (findViewById(R.id.start)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Timer timer = new Timer();
+                timer.scheduleAtFixedRate(new TimerTask() {
+                    @Override
+                    public void run() {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                ((GeneralCounter) findViewById(R.id.general_counter)).increment();
+                            }
+                        });
+                    }
+                }, 1000, 1000);
+                (findViewById(R.id.start)).setClickable(false);
             }
         });
 
         (findViewById(R.id.reset)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                ((GeneralCounter) findViewById(R.id.general_counter)).reset();
             }
         });
     }
